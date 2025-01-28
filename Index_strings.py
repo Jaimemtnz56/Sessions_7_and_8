5e8e4de601ec1ac99a1f94f568d86e473561d1a1
jaimemtnz56 <jaimemtnzpz@icloud.com>
2025-01-28 16:52:00 +0100

    Added index strings examples

---
+++ Index_strings.py
@@ -0,0 +1,8 @@
+s1 = "hello world!"
+print(s1)
+print(s1[0], s1[1], s1[2]) #print characters
+print(s1[4], s1[7]) # print both os
+print(s1[11], s1[-1]) #print the last character
+print(s1[-2]) #count from the end of the string
+#You cannot print for example 15 or 1.2 as it is not an option for the string provided
+print(s1[14//2]) #needed the double // to provide an integer number
