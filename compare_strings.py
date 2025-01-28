953323fed23a5966118aa307f326dd2f1141fb74
jaimemtnz56 <jaimemtnzpz@icloud.com>
2025-01-28 17:47:38 +0100

    Added compare strings and first concept of methods

---
+++ compare_strings.py
@@ -0,0 +1,19 @@
+s1= "banana"
+s2= "banana"
+print(s1 == s2)
+s2= "Banana"
+print(s1 == s2)
+print(s1 > s2) #beacause b is greater than B (lowercase is considered greater)
+s1= "banana"
+s2= "banany"
+print(s1 > s2) #False, because a is not > y (y towards the end of the alphabet)
+
+#in operator can be used for strings
+s1 = "I went to see James"
+s2 = "went"
+print(s2 in s1) #True, it is inside
+print("ana" in "banana") #True, it is inside the word
+print("ANA" in "banana") #False, it is not inside the word, not capital letters
+
+print(dir("I went to see James")) # to see the different methods of a string
+
