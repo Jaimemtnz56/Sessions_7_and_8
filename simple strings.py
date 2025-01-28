a8714b77c0384db1b5e3f65f0f15e009d723a03f
jaimemtnz56 <jaimemtnzpz@icloud.com>
2025-01-28 16:46:33 +0100

    Added simple strings examples

---
+++ simple strings.py
@@ -0,0 +1,12 @@
+s1= "John" #double quote
+s2= 'Mary' #single quote
+print(s1)
+print(s2)
+
+s1= "And then he asked: 'How are you?'" #can also be done the other way around
+print(s1)
+s1= 'And then he asked: "How are you?"' #can also be done the other way around
+print(s1)
+
+s1="This is how to have \" and \' inside a string" #you can also escape them
+print(s1)
