f51efa6585c2ff058787a118c90dd0611d2cb275
jaimemtnz56 <jaimemtnzpz@icloud.com>
2025-01-28 16:56:37 +0100

    Added string operation examples

--- Index_strings.py
+++ Index_strings.py
@@ -6,3 +6,21 @@ print(s1[11], s1[-1]) #print the last character
 print(s1[-2]) #count from the end of the string
 #You cannot print for example 15 or 1.2 as it is not an option for the string provided
 print(s1[14//2]) #needed the double // to provide an integer number
+
+#Operations with strings
+s1= "Hello"
+s2= "world"
+
+print(s1+ " " + s2 + "!") #space and exclamation are also strings
+
+#if operations:
+if "bob":
+    print("bob is")
+else:
+    print("bob isn't") #code unreachable
+
+if "":
+    print("Empty string is true") #code unreachable
+else:
+    print("Empty string is false")
+
